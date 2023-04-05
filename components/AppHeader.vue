a8255d27bd1370252dfca28f55785ab2be1bfaca
André Vandal <me@andrevandal.dev>
2023-04-04 21:44:28 -0300

    feat: implement some histories

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,34 +1,52 @@
 <template>
-  <header class="pt-3 pb-6 bg-gray-900">
-    <nav
-      class="container flex flex-row justify-between max-w-sm gap-2 px-4 mx-auto"
-    >
-      <a href="/" class="flex-1">
-        <span class="sr-only">ondevamos.app</span>
-        <img src="/images/logo-light.svg" class="w-auto h-10" height="40" />
-      </a>
-      <BaseButton icon="log-in" href="#">
-        <span>Entrar</span>
-      </BaseButton>
-    </nav>
-    <section>
-      <div class="container max-w-sm p-4 mx-auto">
+  <header 
+  :class="['py-3 bg-gray-900', {
+    'pb-6': !props.onlyNavigation
+  }]">
+    <AppNavigation :user="user" />
+    <BaseSection v-if="!props.onlyNavigation">
+      <template #before>
         <BaseAd />
-        <div class="flex flex-col gap-2 py-6 text-center">
-          <h1
-            class="text-2xl font-extrabold leading-7 tracking-tighter text-white"
-          >
-            Explore os melhores lugares e encontre a experiência perfeita para
-            você!
-          </h1>
-          <h2 class="text-gray-300">
-            Planeje o seu próximo rolê na companhia que você gosta!
-          </h2>
-        </div>
+      </template>
+      <div class="flex flex-col gap-2 py-6 text-center">
+        <h1
+          class="text-2xl font-extrabold leading-7 tracking-tighter text-white"
+        >
+          Explore os melhores lugares e encontre a experiência perfeita para
+          você!
+        </h1>
+        <h2 class="text-gray-300">
+          Planeje o seu próximo rolê na companhia que você gosta!
+        </h2>
       </div>
-      <BaseSearch />
-    </section>
+      <template #after>
+        <BaseSearch />
+      </template>
+    </BaseSection>
   </header>
 </template>
+
+<script lang="ts" setup>
+import {
+  AppNavigation,
+  BaseSection,
+  BaseAd,
+  BaseSearch,
+} from '#components'
+
+type User = {
+  id: string
+  name: string
+  profilePictureSrc: string
+}
+
+const props = defineProps({
+  onlyNavigation: Boolean,
+  user: {
+    type: Object as PropType<User>,
+    default: () => ({}),
+  },
+})
+</script>
 
 <style></style>
---
+++ stories/components/AppHeader-desktop.story.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+import { AppHeader } from '#components'
+const user = {
+  id: '1',
+  name: 'Suzana',
+  profilePictureSrc: '/images/32.jpeg',
+}
+</script>
+
+<template>
+  <Story
+    title="AppHeader (desktop)"
+    :layout="{
+      type: 'grid',
+      width: '1024px',
+    }"
+  >
+    <Variant title="Guest">
+      <AppHeader />
+    </Variant>
+    <Variant title="Logged">
+      <AppHeader :user="user" />
+    </Variant>
+    <Variant title="Guest (navigation only)">
+      <AppHeader only-navigation />
+    </Variant>
+    <Variant title="Logged (navigation only)">
+      <AppHeader :user="user" only-navigation />
+    </Variant>
+  </Story>
+</template>
