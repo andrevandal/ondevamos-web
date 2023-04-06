963110f6312a5250a8a8c87c44521175a6eb4678
Lucas Ribeiro <lucas.ribeiro711@gmail.com>
2023-04-06 15:02:29 -0300

    feat: implement base ad historie

---
+++ stories/components/BaseAd-desktop.story.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { BaseAd } from '#components'
+</script>
+
+<template>
+  <Story
+    title="BaseAd"
+    :layout="{
+      type: 'grid',
+    }"
+  >
+    <BaseAd />
+  </Story>
+</template>
