88e89c23e1b1f377c410eeb979cea25f02f1ed41
Lucas <lucas.ribeiro711@gmail.com>
2023-05-31 22:12:55 -0300

    chore: places come from outside the component

---
+++ ex.ts
@@ -0,0 +1,52 @@
+export const place = {
+  cover: {
+    url: '/images/375x165.png',
+    alt: '',
+  },
+  title: 'Dom Catulo Burguer',
+  avatar: '/images/100x100.png',
+  available: true,
+  address: 'Rod. Hélio Smidt, Check-in B - Aeroporto, Guarulhos - SP',
+  description: 'Lorem ipsum dolor sit amet consectetur. Venenatis egestas.',
+  rating: 3,
+  price: 3,
+  veganOptions: true,
+  petFriendly: true,
+  featuredMedias: Array(3).fill({
+    type: 'image',
+    src: '/images/270x338.png',
+    alt: '',
+  }),
+  actions: [
+    {
+      icon: 'map-pin-outline',
+      label: 'Como chegar',
+      href: 'https://www.google.com/maps?q=',
+    },
+    {
+      icon: 'instagram',
+      label: '@pedrohenri.ms',
+      href: 'https://www.instagram.com/pedrohenri.ms',
+    },
+    {
+      icon: 'whatsapp',
+      label: '(44) 99144-1919',
+      href: 'https://wa.me/+5544991441919',
+    },
+  ],
+  mainAttractions: Array(2).fill({
+    image: {
+      src: '/images/270x338.png',
+      alt: '',
+    },
+    title: 'Deliciosos Cookies',
+    description: 'A partir de R$00,00',
+  }),
+  openNow: false,
+  openingHours: [
+    { day: 'Segunda-feira', hour: '07:00-23:00' },
+    { day: 'Terça-feira', hour: '07:00-23:00' },
+    { day: 'Quarta-feira', hour: '07:00-23:00' },
+    { day: 'Quinta-feira', hour: '07:00-23:00' },
+  ],
+}
--- pages/company.vue
+++ pages/company.vue
@@ -3,13 +3,17 @@
     <article>
       <div class="flex flex-col">
         <header class="w-full mb-6">
-          <img :src="cover.url" :alt="cover.alt" class="w-full h-auto" />
+          <img
+            :src="place.cover.url"
+            :alt="place.cover.alt"
+            class="w-full h-auto"
+          />
           <div
             class="relative rounded-full flex items-end gap-4 w-[100px] h-[100px] -translate-y-1/2 -mb-[50px] z-10 border-4 border-white ml-4"
           >
             <img
-              :src="avatar"
-              :alt="title"
+              :src="place.avatar"
+              :alt="place.title"
               class="rounded-full"
               width="100"
               height="100"
@@ -18,7 +22,7 @@
               :class="[
                 'absolute bg-[#0D8047] w-5 h-5 border-2 border-white rounded-full bottom-0 right-0',
                 {
-                  hidden: !available,
+                  hidden: !place.available,
                 },
               ]"
             >
@@ -26,26 +30,26 @@
             </div>
             <span class="h-9">
               Avaliação
-              <BaseRating :level="rating" />
+              <BaseRating :level="place.rating" />
             </span>
             <span class="h-9">
               Preço
-              <BasePriceLevel :level="price" />
+              <BasePriceLevel :level="place.price" />
             </span>
           </div>
         </header>
         <div class="px-4 pb-14">
           <h1 class="pb-2 text-2xl font-semibold leading-7 text-gray-900">
-            {{ title }}
+            {{ place.title }}
           </h1>
           <a
             href="#"
             class="text-blue text-sm block leading-4 max-w-[calc(100%-.5rem)] text-ellipsis overflow-hidden whitespace-nowrap pb-2"
-            >{{ address }}</a
+            >{{ place.address }}</a
           >
           <div class="text-gray-500 text-xs space-x-2 pt-3">
             <span
-              v-if="veganOptions"
+              v-if="place.veganOptions"
               class="inline-flex items-center gap-1 px-3 py-1 border border-gray-100 rounded-full"
             >
               <NuxtIcon name="veggan" />
@@ -53,7 +57,7 @@
               <NuxtIcon name="check" class="text-[#009959]" />
             </span>
             <span
-              v-if="petFriendly"
+              v-if="place.petFriendly"
               class="inline-flex items-center gap-1 px-3 py-1 border border-gray-100 rounded-full"
             >
               <NuxtIcon name="pet" />
@@ -63,10 +67,13 @@
           </div>
           <div class="flex-col gap-1 py-4">
             <h2 class="font-bold leading-5 text-gray-900">Sobre</h2>
-            <p class="leading-5 text-gray-500">{{ description }}</p>
+            <p class="leading-5 text-gray-500">{{ place.description }}</p>
           </div>
           <ul class="flex flex-row gap-1 mb-8">
-            <li v-for="(action, index) in actions" :key="`action-${index}`">
+            <li
+              v-for="(action, index) in place.actions"
+              :key="`action-${index}`"
+            >
               <a
                 href="#"
                 :class="[
@@ -106,12 +113,12 @@
               <span
                 :class="[
                   'font-bold text-[#009959]',
-                  { ['text-[#D44431]']: !openNow },
+                  { ['text-[#D44431]']: !place.openNow },
                 ]"
-                >{{ openNow ? 'Aberto Agora' : 'Fechado Agora' }}</span
+                >{{ place.openNow ? 'Aberto Agora' : 'Fechado Agora' }}</span
               >
               <span>
-                {{ openNow ? 'Fecha às' : 'Abre às' }} : uma hora ai
+                {{ place.openNow ? 'Fecha às' : 'Abre às' }} : uma hora ai
               </span>
               <button
                 :class="[
@@ -128,7 +135,7 @@
               class="absolute inset-x-0 p-4 flex flex-col gap-2 bg-white w-[342px] shadow-lg rounded-lg"
             >
               <li
-                v-for="(hour, index) in openingHours"
+                v-for="(hour, index) in place.openingHours"
                 :key="index"
                 class="flex py-2 px-4 bg-gray-50 rounded-lg"
               >
@@ -148,7 +155,9 @@
         >
           <template #default="{ isMouseDown }">
             <img
-              v-for="(featuredMedia, featuredMediaIndex) in featuredMedias"
+              v-for="(
+                featuredMedia, featuredMediaIndex
+              ) in place.featuredMedias"
               :key="`featured-media-index-${featuredMediaIndex}`"
               :src="featuredMedia.src"
               :alt="featuredMedia.alt"
@@ -168,7 +177,7 @@
             </h2>
             <ul class="flex flex-col">
               <li
-                v-for="(attraction, attractionIndex) in mainAttractions"
+                v-for="(attraction, attractionIndex) in place.mainAttractions"
                 :key="`attraction-index-${attractionIndex}`"
                 class="flex flex-row py-2 gap-x-4"
               >
@@ -199,6 +208,7 @@
 <script lang="ts">
 import { PerfectScrollbar } from 'vue3-perfect-scrollbar'
 import { BaseHorizontalScroll } from '#components'
+import { place } from '@/ex'
 import 'vue3-perfect-scrollbar/dist/vue3-perfect-scrollbar.css'
 
 export default defineComponent({
@@ -210,6 +220,11 @@ export default defineComponent({
       type: [String, Array, null] as PropType<String | String[] | null>,
       default: '',
     },
+    // place: {
+    //   type: [String, Array, null] as PropType<String | String[] | null>, TODO: precisa ser discutido
+    //   default: '',
+    //   requierd: true,
+    // },
   },
   setup(_props) {
     const scrollInstance = ref<typeof BaseHorizontalScroll | null>(null)
@@ -226,56 +241,7 @@ export default defineComponent({
     // https://schema.org/openingHours
     // Mo-Su 9:00-13:00 16:00-20:00
     return {
-      cover: {
-        url: '/images/375x165.png',
-        alt: '',
-      },
-      title: 'Dom Catulo Burguer',
-      avatar: '/images/100x100.png',
-      available: true,
-      address: 'Rod. Hélio Smidt, Check-in B - Aeroporto, Guarulhos - SP',
-      description: 'Lorem ipsum dolor sit amet consectetur. Venenatis egestas.',
-      rating: 3,
-      price: 3,
-      veganOptions: true,
-      petFriendly: true,
-      featuredMedias: Array(3).fill({
-        type: 'image',
-        src: '/images/270x338.png',
-        alt: '',
-      }),
-      actions: [
-        {
-          icon: 'map-pin-outline',
-          label: 'Como chegar',
-          href: 'https://www.google.com/maps?q=',
-        },
-        {
-          icon: 'instagram',
-          label: '@pedrohenri.ms',
-          href: 'https://www.instagram.com/pedrohenri.ms',
-        },
-        {
-          icon: 'whatsapp',
-          label: '(44) 99144-1919',
-          href: 'https://wa.me/+5544991441919',
-        },
-      ],
-      mainAttractions: Array(2).fill({
-        image: {
-          src: '/images/270x338.png',
-          alt: '',
-        },
-        title: 'Deliciosos Cookies',
-        description: 'A partir de R$00,00',
-      }),
-      openNow: false,
-      openingHours: [
-        { day: 'Segunda-feira', hour: '07:00-23:00' },
-        { day: 'Terça-feira', hour: '07:00-23:00' },
-        { day: 'Quarta-feira', hour: '07:00-23:00' },
-        { day: 'Quinta-feira', hour: '07:00-23:00' },
-      ],
+      place,
       toggleModal,
       openAtModalRef,
       openAtModalVisible,
